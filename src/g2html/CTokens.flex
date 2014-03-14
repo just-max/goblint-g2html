05c4667ee3c0a974aaa6c16979a455ec63b3e85e
Kalmer Apinis <kalmer.apinis@gmail.com>
2014-03-14 14:48:17 +0100

    added more syntax highlighting

---
+++ src/g2html/CTokens.flex
@@ -0,0 +1,209 @@
+package g2html;
+
+%%
+
+%class CTokens
+%public
+%final
+%apiprivate
+%function next_token1
+%type Symbol
+%unicode
+%char
+%line
+%column
+
+%{
+
+  public enum Kind { LPAREN, RPAREN, LBRACE, RBRACE, LBRACK, RBRACK, SEMICOLON, COMMA, DOT, EQ, GT, LT, NOT, COMP,
+                     QUESTION, COLON, EQEQ, LTEQ, GTEQ, NOTEQ, ANDAND, OROR, PLUSPLUS, MINUSMINUS, PLUS, MINUS, 
+                     MULT, DIV, AND, OR, XOR, MOD, LSHIFT, RSHIFT, URSHIFT, PLUSEQ, MINUSEQ, MULTEQ, DIVEQ, 
+                     ANDEQ, OREQ, XOREQ, MODEQ, LSHIFTEQ, RSHIFTEQ, URSHIFTEQ, ASM, AUTO, BREAK, CASE, CHAR, CONST,
+                     CONTINUE, DEFAULT, DO, DOUBLE, ELSE, ENUM, EXTERN, FLOAT, FOR, GOTO, IF, INT, LONG, REGISTER,
+                     RETURN, SHORT, SIGNED, SIZEOF, STATIC, STRUCT, SWITCH, TYPEDEF, UNION, UNSIGNED, VOID, VOLATILE, 
+                     WHILE, TEXT, EOL, EOF, STRING, STRING_CONT, COMMENT, COMMENT_CONT, PPROC, NUMBER, WHITESP, CHARLIT }
+
+  class Symbol {
+    public Kind k;
+    public String s;
+    public Symbol(Kind k, String s){
+      this.k = k;
+      this.s = s;
+    }
+    public String toString(){
+      return k.toString()+s;
+    }
+  }
+
+  private Symbol symbol(Kind k, String s){
+    return new Symbol(k,s);
+  }
+  
+  private Symbol symbol(Kind k){
+    return new Symbol(k, yytext());
+  }
+  
+  public Symbol next_token() throws java.io.IOException {
+    return next_token1();
+  }
+
+  StringBuilder string = new StringBuilder();
+  
+%}
+
+/* main character classes */
+LineTerminator = \r|\n|\r\n
+InputCharacter = [^\r\n]
+
+WhiteSpace = [ \t\f]
+
+/* identifiers */
+Identifier = [:jletter:][:jletterdigit:]*
+
+/* integer literals */
+DecIntegerLiteral = 0 | [1-9][0-9]*
+DecLongLiteral    = {DecIntegerLiteral} [lL]
+
+HexIntegerLiteral = 0 [xX] 0* {HexDigit} {1,8}
+HexLongLiteral    = 0 [xX] 0* {HexDigit} {1,16} [lL]
+HexDigit          = [0-9a-fA-F]
+    
+/* floating point literals */        
+FloatLiteral  = ({FLit1}|{FLit2}|{FLit3}) {Exponent}? [fF]
+DoubleLiteral = ({FLit1}|{FLit2}|{FLit3}) {Exponent}?
+
+FLit1    = [0-9]+ \. [0-9]* 
+FLit2    = \. [0-9]+ 
+FLit3    = [0-9]+ 
+Exponent = [eE] [+-]? [0-9]+
+
+Number  = {FloatLiteral} | {DoubleLiteral} | {DecIntegerLiteral} | {DecLongLiteral} | {HexLongLiteral} | {HexIntegerLiteral}
+
+
+%state STRING, CHARLITERAL, COMMENT
+
+%%
+<YYINITIAL> {
+  {WhiteSpace}+                              { return symbol(Kind.WHITESP);   }
+                                                                              
+  "#include"{WhiteSpace}+"<"[^">"]+">"       { return symbol(Kind.PPROC);     }
+  
+  #{Identifier}+                             { return symbol(Kind.PPROC);     }
+                                                                              
+  /* keywords */                                                              
+  "asm"                                      { return symbol(Kind.ASM);       }     
+  "auto"                                     { return symbol(Kind.AUTO);      }   
+  "break"                                    { return symbol(Kind.BREAK);     }   
+  "case"                                     { return symbol(Kind.CASE);      }   
+  "char"                                     { return symbol(Kind.CHAR);      }   
+  "const"                                    { return symbol(Kind.CONST);     }   
+  "continue"                                 { return symbol(Kind.CONTINUE);  }       
+  "default"                                  { return symbol(Kind.DEFAULT);   }     
+  "do"                                       { return symbol(Kind.DO);        } 
+  "double"                                   { return symbol(Kind.DOUBLE);    }     
+  "else"                                     { return symbol(Kind.ELSE);      }   
+  "enum"                                     { return symbol(Kind.ENUM);      }   
+  "extern"                                   { return symbol(Kind.EXTERN);    }     
+  "float"                                    { return symbol(Kind.FLOAT);     }   
+  "for"                                      { return symbol(Kind.FOR);       } 
+  "goto"                                     { return symbol(Kind.GOTO);      }   
+  "if"                                       { return symbol(Kind.IF);        } 
+  "int"                                      { return symbol(Kind.INT);       } 
+  "long"                                     { return symbol(Kind.LONG);      }   
+  "register"                                 { return symbol(Kind.REGISTER);  }       
+  "return"                                   { return symbol(Kind.RETURN);    }     
+  "short"                                    { return symbol(Kind.SHORT);     }   
+  "signed"                                   { return symbol(Kind.SIGNED);    }     
+  "sizeof"                                   { return symbol(Kind.SIZEOF);    }     
+  "static"                                   { return symbol(Kind.STATIC);    }     
+  "struct"                                   { return symbol(Kind.STRUCT);    }     
+  "switch"                                   { return symbol(Kind.SWITCH);    }     
+  "typedef"                                  { return symbol(Kind.TYPEDEF);   }     
+  "union"                                    { return symbol(Kind.UNION);     }   
+  "unsigned"                                 { return symbol(Kind.UNSIGNED);  }       
+  "void"                                     { return symbol(Kind.VOID);      }   
+  "volatile"                                 { return symbol(Kind.VOLATILE);  }       
+  "while"                                    { return symbol(Kind.WHILE);     }   
+  
+  /* separators */
+  "("                                        { return symbol(Kind.LPAREN);    }
+  ")"                                        { return symbol(Kind.RPAREN);    }
+  "{"                                        { return symbol(Kind.LBRACE);    }
+  "}"                                        { return symbol(Kind.RBRACE);    }
+  "["                                        { return symbol(Kind.LBRACK);    }
+  "]"                                        { return symbol(Kind.RBRACK);    }
+  ";"                                        { return symbol(Kind.SEMICOLON); }
+  ","                                        { return symbol(Kind.COMMA);     }
+  "."                                        { return symbol(Kind.DOT);       }
+                                            
+  /* operators */                            
+  "="                                        { return symbol(Kind.EQ); }
+  ">"                                        { return symbol(Kind.GT); }
+  "<"                                        { return symbol(Kind.LT); }
+  "!"                                        { return symbol(Kind.NOT); }
+  "~"                                        { return symbol(Kind.COMP); }
+  "?"                                        { return symbol(Kind.QUESTION); }
+  ":"                                        { return symbol(Kind.COLON); }
+  "=="                                       { return symbol(Kind.EQEQ); }
+  "<="                                       { return symbol(Kind.LTEQ); }
+  ">="                                       { return symbol(Kind.GTEQ); }
+  "!="                                       { return symbol(Kind.NOTEQ); }
+  "&&"                                       { return symbol(Kind.ANDAND); }
+  "||"                                       { return symbol(Kind.OROR); }
+  "++"                                       { return symbol(Kind.PLUSPLUS); }
+  "--"                                       { return symbol(Kind.MINUSMINUS); }
+  "+"                                        { return symbol(Kind.PLUS); }
+  "-"                                        { return symbol(Kind.MINUS); }
+  "*"                                        { return symbol(Kind.MULT); }
+  "/"                                        { return symbol(Kind.DIV); }
+  "&"                                        { return symbol(Kind.AND); }
+  "|"                                        { return symbol(Kind.OR); }
+  "^"                                        { return symbol(Kind.XOR); }
+  "%"                                        { return symbol(Kind.MOD); }
+  "<<"                                       { return symbol(Kind.LSHIFT); }
+  ">>"                                       { return symbol(Kind.RSHIFT); }
+  ">>>"                                      { return symbol(Kind.URSHIFT); }
+  "+="                                       { return symbol(Kind.PLUSEQ); }
+  "-="                                       { return symbol(Kind.MINUSEQ); }
+  "*="                                       { return symbol(Kind.MULTEQ); }
+  "/="                                       { return symbol(Kind.DIVEQ); }
+  "&="                                       { return symbol(Kind.ANDEQ); }
+  "|="                                       { return symbol(Kind.OREQ); }
+  "^="                                       { return symbol(Kind.XOREQ); }
+  "%="                                       { return symbol(Kind.MODEQ); }
+  "<<="                                      { return symbol(Kind.LSHIFTEQ); }
+  ">>="                                      { return symbol(Kind.RSHIFTEQ); }
+  
+  /* string literal */
+  \"                                         { yybegin(STRING); string.setLength(0); string.append("\""); }
+                                            
+  /* character literal */                   
+  \'[^\']+\'                                 { return symbol(Kind.CHARLIT); }
+
+  /* comments */
+  "//" {InputCharacter}*                     { return symbol(Kind.COMMENT); }
+  "/*"                                       { yybegin(COMMENT); string.setLength(0); string.append("/*"); }
+  
+  {Number}                                   { return symbol(Kind.NUMBER); }
+
+  {Identifier}                               { return symbol(Kind.TEXT); }
+  
+  {LineTerminator}                           { return symbol(Kind.EOL);  }
+}                                            
+                                             
+<STRING> {                                   
+  \"                                         { yybegin(YYINITIAL); string.append("\""); return symbol(Kind.STRING, string.toString()); }
+  [^\"\n\r]*                                 { string.append( yytext() ); }
+  {LineTerminator}                           { return symbol(Kind.STRING_CONT, string.toString()); }
+}                                            
+                                             
+                                             
+<COMMENT> {                                  
+  "*/"                                       { yybegin(YYINITIAL); string.append("*/"); return symbol(Kind.COMMENT, string.toString()); }
+  [^*\r\n]*                                  { string.append( yytext() ); }
+  "*"[^/]                                    { string.append( yytext() ); }
+  {LineTerminator}                           { return symbol(Kind.COMMENT_CONT, string.toString()); }
+}                                            
+                                             
+{InputCharacter}                             { return symbol(Kind.TEXT); }        
+<<EOF>>                                      { return symbol(Kind.EOF);            }
--- src/g2html/ProcessCfile.java
+++ src/g2html/ProcessCfile.java
@@ -3,8 +3,17 @@ package g2html;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.TreeSet;
+
+import static g2html.CTokens.Kind;
 
 public class ProcessCfile implements Runnable {
 	private File to;
@@ -37,7 +46,80 @@ public class ProcessCfile implements Runnable {
 		return stringBuilder.toString();
 	}
 
-	
+	private static Set<Kind> type_kw = new TreeSet<>(Arrays.asList(new Kind[]{Kind.CHAR, Kind.CONST, Kind.DOUBLE, Kind.AUTO, Kind.ENUM,
+        Kind.EXTERN, Kind.FLOAT, Kind.INT, Kind.LONG, Kind.REGISTER, Kind.SHORT, Kind.SIGNED, Kind.STATIC, Kind.STRUCT, 
+        Kind.TYPEDEF, Kind.UNION, Kind.UNSIGNED, Kind.VOID, Kind.VOLATILE}));
+
+	private static Set<Kind> stmt_kw = new TreeSet<>(Arrays.asList(new Kind[]{Kind.ASM, Kind.BREAK, Kind.CASE, Kind.CONTINUE,
+        Kind.DEFAULT, Kind.DO, Kind.ELSE, Kind.FOR, Kind.GOTO, Kind.IF, Kind.RETURN, Kind.SIZEOF, Kind.SWITCH, 
+        Kind.WHILE}));
+
+	private static Set<Kind> operatr = new TreeSet<>(Arrays.asList(new Kind[]{	Kind.DOT, Kind.EQ, Kind.GT, Kind.LT, Kind.NOT, Kind.COMP,
+				Kind.QUESTION, Kind.COLON, Kind.EQEQ, Kind.LTEQ, Kind.GTEQ, Kind.NOTEQ, Kind.ANDAND, Kind.OROR, Kind.PLUSPLUS, 
+        Kind.MINUSMINUS, Kind.PLUS, Kind.MINUS, Kind.MULT, Kind.DIV, Kind.AND, Kind.OR, Kind.XOR, Kind.MOD, 
+        Kind.LSHIFT, Kind.RSHIFT, Kind.URSHIFT, Kind.PLUSEQ, Kind.MINUSEQ, Kind.MULTEQ, Kind.DIVEQ,
+				Kind.ANDEQ, Kind.OREQ, Kind.XOREQ, Kind.MODEQ, Kind.LSHIFTEQ, Kind.RSHIFTEQ, Kind.URSHIFTEQ}));
+
+	private static Set<Kind> separtr = new TreeSet<>(Arrays.asList(new Kind[]{Kind.LPAREN, Kind.RPAREN, Kind.LBRACE, Kind.RBRACE,
+          Kind.LBRACK, Kind.RBRACK, Kind.SEMICOLON, Kind.COMMA}));
+
+	private static Set<Kind> comment = new TreeSet<>(Arrays.asList(new Kind[]{Kind.COMMENT, Kind.COMMENT_CONT}));
+
+	private static Set<Kind> strings = new TreeSet<>(Arrays.asList(new Kind[]{Kind.STRING, Kind.STRING_CONT, Kind.CHARLIT}));
+
+
+	private static void kind_wrap_start(Kind k, XMLStreamWriter sw) throws XMLStreamException {
+		if (k==Kind.TEXT) {
+
+		} else if (k==Kind.NUMBER){
+			sw.writeStartElement("sht");
+			sw.writeAttribute("type" , "nr");
+		} else if (k==Kind.PPROC) {
+			sw.writeStartElement("sht");
+			sw.writeAttribute("type" , "pp");
+		} else if (type_kw.contains(k)) {
+			sw.writeStartElement("sht");
+			sw.writeAttribute("type" , "tk");
+		} else if (stmt_kw.contains(k)) {
+			sw.writeStartElement("sht");
+			sw.writeAttribute("type" , "sk");
+		} else if (operatr.contains(k)) {
+			sw.writeStartElement("sht");
+			sw.writeAttribute("type" , "op");
+		} else if (separtr.contains(k)) {
+			sw.writeStartElement("sht");
+			sw.writeAttribute("type" , "sp");
+		} else if (comment.contains(k)) {
+			sw.writeStartElement("sht");
+			sw.writeAttribute("type" , "cm");
+		} else if (strings.contains(k)){
+			sw.writeStartElement("sht");
+			sw.writeAttribute("type" , "st");
+		}
+	}
+
+	private static void kind_wrap_end(Kind k, XMLStreamWriter sw) throws XMLStreamException {
+		if (k==Kind.TEXT) {
+
+		} else if (k==Kind.NUMBER){
+			sw.writeEndElement();
+		} else if (k==Kind.PPROC) {
+			sw.writeEndElement();
+		} else if (type_kw.contains(k)) {
+			sw.writeEndElement();
+		} else if (stmt_kw.contains(k)) {
+			sw.writeEndElement();
+		} else if (operatr.contains(k)) {
+			sw.writeEndElement();
+		} else if (separtr.contains(k)) {
+			sw.writeEndElement();
+		} else if (comment.contains(k)) {
+			sw.writeEndElement();
+		} else if (strings.contains(k)){
+			sw.writeEndElement();
+		}
+	}
+
 	@Override
 	public void run() {
 		Log.printf("Starting:%s.\n", from.getPath());
@@ -50,38 +132,48 @@ public class ProcessCfile implements Runnable {
 				XMLStreamWriter sw = outFactory.createXMLStreamWriter(new FileOutputStream(to));
 				// write the preamble
 				sw.writeStartDocument();
-  				sw.writeCharacters("\n");
- 	  			sw.writeProcessingInstruction("xml-stylesheet", "type=\"text/xsl\" href=\"../file.xsl\"");
+				sw.writeCharacters("\n");
+				sw.writeProcessingInstruction("xml-stylesheet", "type=\"text/xsl\" href=\"../file.xsl\"");
 				sw.writeCharacters("\n");
 				sw.writeStartElement("file");
 				sw.writeCharacters("\n");
 
-				isMultilineCommentActive = false;
-				isMultilineStringActive = false;
 
 				// for each line write a ln tag with line number, nodes, warnings, and reachability
 				int lineNr = 1;
 				try (BufferedReader br = new BufferedReader(new FileReader(from))) {
-					for (String line; (line = br.readLine()) != null; ) {
+					CTokens scanner = null;
+					scanner = new CTokens(br);
+					do {
 						sw.writeStartElement("ln");
 						sw.writeAttribute("nr" , Integer.toString(lineNr));
 						sw.writeAttribute("ns" , setToJsonArray(stats.getLineData(lineNr)));
 						sw.writeAttribute("wrn", setToJsonArray(stats.getWarnData(lineNr)));
 						sw.writeAttribute("ded", Boolean.toString(stats.isDead(lineNr)));
 
-						// Disabled syntax highlighting
-						if (Config.conf.getSyntaxHighlightingStatus() == "off") {
-							sw.writeCharacters(line);
-						}
-						// Enabled syntax highlighting
-						else {
-							writeSyntaxHighlightedLinePart(sw,line);
-						}
+						CTokens.Symbol s;
+						do {
+							s = scanner.next_token();
+							if (s.k == Kind.EOL || s.k == Kind.EOF)
+								break;
+							kind_wrap_start(s.k,sw);
+							sw.writeCharacters(s.s);
+							kind_wrap_end(s.k, sw);
+						} while (true);
 
 						sw.writeEndElement();
 						sw.writeCharacters("\n");
 						lineNr++;
-					}
+
+						if (s.k== Kind.EOF)
+							break;
+					} while (true);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (XMLStreamException e) {
+					e.printStackTrace();
 				}
 
 				// close the stream
@@ -94,194 +186,10 @@ public class ProcessCfile implements Runnable {
 
 			} catch (XMLStreamException e) {
 				e.printStackTrace();
-  			}
+			}
 
 		}
 		Log.printf("Finished:%s.\n", from.getPath());
 	}
-
-
-	// Syntax highlighting code
-
-	private boolean isMultilineCommentActive = false;
-	private boolean isMultilineStringActive = false;
-	static private String[] keywordList = {"const", "auto", "default", "extern", "typedef", "union", "class", 
-					       "register", "struct", "volatile", "enum", "static", "sizeof",
-                                               "signed", "unsigned", "int", "long", "float", "double", "void", "char", "short",
-					       "return", "continue", "break", "if", "else", "do", "while", "switch", "for", "case", "goto"};
-
-	int uint_min(int a, int b) {
-		if (a < 0) return b;
-		if (b < 0) return a;
-		if (a < b) return a;
-		else return b;
-	}
-
-	void writeSyntaxHighlightedLinePart(XMLStreamWriter sw, String line)
-	{
-		try {
-			// if active multiline comment, check if it ends
-			if (isMultilineCommentActive == true) {
-				int endCmtPos = line.indexOf("*/");
-				if (endCmtPos == -1) {  // Complete line is comment
-					sw.writeStartElement("sht");
-					sw.writeAttribute("type", "cmt");
-					sw.writeCharacters(line);
-					sw.writeEndElement();
-					return;
-				}
-				else {
-					sw.writeStartElement("sht");
-					sw.writeAttribute("type", "cmt");
-					sw.writeCharacters(line.substring(0, endCmtPos+2));
-					sw.writeEndElement();
-					isMultilineCommentActive = false;
-					writeSyntaxHighlightedLinePart(sw, line.substring(endCmtPos+2));
-					return;
-				}
-			}
-
-			// if active multiline string, check if it ends
-			if (isMultilineStringActive == true) {
-				int endStrPos = line.indexOf("\"");
-				if (endStrPos == -1) {
-					sw.writeStartElement("sht");
-					sw.writeAttribute("type", "str");
-					sw.writeCharacters(line);
-					sw.writeEndElement();
-					if (line.charAt(line.length()-1) != '\\') {
-						isMultilineStringActive = false;
-					}
-				}
-				else {	// String is finished
-					sw.writeStartElement("sht");
-					sw.writeAttribute("type", "str");
-					sw.writeCharacters(line.substring(0, endStrPos+1));
-					sw.writeEndElement();
-					isMultilineStringActive = false;
-					writeSyntaxHighlightedLinePart(sw, line.substring(endStrPos+1));
-				}
-				return;
-			}
-
-			// Get first important character
-			int firstCommentPos = line.indexOf("//");
-			int firstMultilineCommentPos = line.indexOf("/*");
-			int firstStringPos = line.indexOf("\"");
-			int firstCharPos = line.indexOf("'");
-			int firstSharpPos = line.indexOf("#");
-			int firstPos = uint_min(firstCommentPos, uint_min(firstMultilineCommentPos, uint_min(firstStringPos, uint_min(firstCharPos, firstSharpPos))));
-			int endPos = line.length()-1;
-
-			// Write characters without any syntax highlighting for strings, comments, ...
-			if (firstPos == -1) {
-				// Find the first keyword in the line
-				for (int i = 0; i < keywordList.length; i++) {
-					int keywordPos = line.indexOf(keywordList[i]);
-					if (keywordPos == -1) continue;
-					int keywordLen = keywordList[i].length();
-
-					// No letters or digits before and after the keyword
-					if (keywordPos > 0) if (Character.isLetterOrDigit(line.charAt(keywordPos-1)) == true) continue;
-					if (line.length() > keywordPos+keywordLen) if (Character.isLetterOrDigit(line.charAt(keywordPos+keywordLen)) == true) continue;
-
-					// Highlight keyword and recursive call to the before and after part
-					if (keywordPos > 0) writeSyntaxHighlightedLinePart(sw, line.substring(0, keywordPos));
-					sw.writeStartElement("sht");
-					sw.writeAttribute("type", "key");
-					sw.writeCharacters(line.substring(keywordPos, keywordPos+keywordLen));
-					sw.writeEndElement();					
-					if (line.length() > keywordPos+keywordLen-1) writeSyntaxHighlightedLinePart(sw, line.substring(keywordPos+keywordLen));
-
-					return;
-				}
-
-				sw.writeCharacters(line);
-				return;
-			}
-
-			// Write characters before syntax characters
-			if (firstPos > 0) writeSyntaxHighlightedLinePart(sw, line.substring(0, firstPos));
-
-			// Preprocessor starts
-			if (firstPos == firstSharpPos) {
-				sw.writeStartElement("sht");
-				sw.writeAttribute("type", "pp");
-				sw.writeCharacters(line.substring(firstPos));	// All characters following the "#"
-				sw.writeEndElement();
-				return;
-			}
-
-			// Comment starts
-			if (firstPos == firstCommentPos) {
-				sw.writeStartElement("sht");
-				sw.writeAttribute("type", "cmt");
-				sw.writeCharacters(line.substring(firstPos));	// All characters following the "//"
-				sw.writeEndElement();
-				return;
-			}
-
-			// Character starts
-			if (firstPos == firstCharPos) {
-				endPos = line.indexOf("'", firstPos+1);
-				if (endPos == -1) {
-					sw.writeCharacters(line.substring(firstPos));
-					return;
-				}
-				sw.writeStartElement("sht");
-				sw.writeAttribute("type", "chr");
-				sw.writeCharacters(line.substring(firstPos, endPos+1));
-				sw.writeEndElement();
-			}
-
-			// String starts
-			if (firstPos == firstStringPos) {
-				endPos = line.indexOf("\"", firstPos+1);
-				if (endPos == -1) {
-					if (line.charAt(line.length()-1) == '\\') {
-						sw.writeStartElement("sht");
-						sw.writeAttribute("type", "str");
-						sw.writeCharacters(line.substring(firstPos));
-						sw.writeEndElement();
-						isMultilineStringActive = true;
-					}
-					else sw.writeCharacters(line.substring(firstPos));
-					return;
-				}
-				sw.writeStartElement("sht");
-				sw.writeAttribute("type", "str");
-				sw.writeCharacters(line.substring(firstPos, endPos+1));
-				sw.writeEndElement();
-			}
-
-			// Multiline comment
-			if (firstPos == firstMultilineCommentPos) {
-				endPos = line.indexOf("*/", firstPos+1);
-				if (endPos > firstPos) { // Comment ends in this line
-					sw.writeStartElement("sht");
-					sw.writeAttribute("type", "cmt");
-					sw.writeCharacters(line.substring(firstPos, endPos+2));
-					sw.writeEndElement();
-					endPos++;
-				}
-				else {	// Comment does not end in this line
-					sw.writeStartElement("sht");
-					sw.writeAttribute("type", "cmt");
-					sw.writeCharacters(line.substring(firstPos));
-					sw.writeEndElement();
-					isMultilineCommentActive = true;
-					return;
-				}
-			}
-
-			// Parse the string part after the highlighted part
-			if (endPos < line.length()) writeSyntaxHighlightedLinePart(sw, line.substring(endPos+1));
-
-		} catch (XMLStreamException e) {
-			e.printStackTrace();
-  		}
-	}
-
-	
 
 }
